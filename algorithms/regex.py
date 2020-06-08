3d8cd0234605eaba1356fc433aa2d7dd90024bd2
godshade <56820040+godshade@users.noreply.github.com>
2020-06-08 17:15:04 +0100

    Delete regex.py

--- algorithms/regex.py
+++
@@ -1,9 +0,0 @@
-import re
-
-email = re.compile(r'[A-Z][A-Za-z0-9]*@[A-Za-z]+\.[a-z]{2,4}')
-
-print(email.findall("My name is Ayush Bhusal and my email address is Bhusalayush@gmail.com. I also like to be called A@g.com."))
-
-replacer = re.compile(r'[A-Z][A-Za-z0-9]*@')
-print(replacer.sub("****@","My name is Ayush Bhusal and my email address is Bhusalayush@gmail.com. I also like to be called A@g.com."))
-
